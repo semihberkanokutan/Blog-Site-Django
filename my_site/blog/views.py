6ad689d6100119c4910901043ffb30e395791670
Semih Berkan Okutan <okutansemih11@gmail.com>
2025-03-21 05:28:07 +0300

    Add templates and static files

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'blog/index.html')
+
+
+def all_blog_posts(request):
+    return HttpResponse('All Blog Posts')
