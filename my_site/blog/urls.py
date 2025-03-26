66e66f94934800dc664351400511d3ed098f65ba
Semih Berkan Okutan <okutansemih11@gmail.com>
2025-03-26 22:56:33 +0300

    Set dynamic url for index and posts page.

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('posts/', views.all_blog_posts)
+    path('', views.index, name="main-page"),
+    path('posts/', views.all_blog_posts, name='all-posts-page')
 ]
